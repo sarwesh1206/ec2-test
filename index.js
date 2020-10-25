a61732104128126d4b370a22e710c5b475694bba
Sachin <sachin.co>
2020-10-25 17:25:23 +0530

    second

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('heello world');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Running On Port PORT`));     
